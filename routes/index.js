7fdd510f6008552e47f47ec3aa81aa5250c3a777
Robert Dennis <rdennis@doctorlogic.com>
2017-01-05 16:56:40 -0600

    Using better stringifier, with indent

--- routes/index.js
+++ routes/index.js
@@ -87,7 +87,7 @@ module.exports = function (app, addon) {
           response += 'Rolled ' + result.result;
 
           if (verbose) {
-            response += '\nResult object:\n' + stringify(result, { maxLength: Infinity });
+            response += '\nResult object:\n' + stringify(result, { maxLength: Infinity, indent: indent });
           }
         }
       }
